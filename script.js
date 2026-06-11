896e4c0eedc00d44362e4a60c934e75effecb52e
Andrey <alenakuznecova80586@gmail.com>
2026-06-11 19:35:37 +0300

    добавил еще кнопок и новый функционал

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const display = document.querySelector("#display");
-const buttons = document.querySelector("#buttons");
+const buttons = document.querySelector(".calculator");
+const extraButtons = document.querySelector("#extra-buttons");
 
 let expression = "";
 
@@ -12,6 +13,26 @@ function isOperator(symbol) {
     );
 }
 
+function updateDisplay() {
+
+    display.value = expression;
+
+    display.scrollLeft = display.scrollWidth;
+
+    if (display.value.length > 20) {
+        display.style.fontSize = "30px";
+    }
+
+    else if (display.value.length > 10) {
+        display.style.fontSize = "40px";
+    }
+
+    else {
+        display.style.fontSize = "60px";
+    }
+
+}
+
 buttons.addEventListener("click", (event) => {
 
     if (event.target.tagName !== "BUTTON") {
@@ -19,7 +40,7 @@ buttons.addEventListener("click", (event) => {
     }
 
     let value = event.target.textContent;
-
+    console.log("[" + value + "]");
     if (value === ",") {
         value = ".";
     }
@@ -27,14 +48,14 @@ buttons.addEventListener("click", (event) => {
     if (value === "AC") {
 
         expression = "";
-        display.value = "";
+        updateDisplay();
 
     }
 
     else if (value === "DEL") {
 
         expression = expression.slice(0, -1);
-        display.value = expression;
+        updateDisplay();
 
     }
 
@@ -43,7 +64,7 @@ buttons.addEventListener("click", (event) => {
         if (expression !== "") {
 
             expression = "(-" + expression + ")";
-            display.value = expression;
+            updateDisplay();
 
         }
 
@@ -54,7 +75,98 @@ buttons.addEventListener("click", (event) => {
         if (expression !== "") {
 
             expression = "(" + expression + "/100)";
-            display.value = expression;
+            updateDisplay();
+
+        }
+
+    }
+
+    else if (value === "MORE") {
+
+        if (extraButtons.classList.contains("hidden")) {
+            extraButtons.classList.remove("hidden");
+        } else {
+            extraButtons.classList.add("hidden");
+        }
+
+    }
+
+    else if (value === "RAND") {
+
+        const randomNumber =
+            "0." +
+            Math.floor(Math.random() * 100000)
+                .toString()
+                .padStart(5, "0");
+
+        expression += randomNumber;
+        updateDisplay();
+
+    }
+
+    else if (value === "π") {
+
+        expression += Math.PI;
+        updateDisplay();
+
+    }
+
+    else if (value === "MR") {
+
+        expression += "*0";
+        updateDisplay();
+
+    }
+
+    else if (value === "x²") {
+
+        if (expression !== "") {
+
+            try {
+
+                const current =
+                    Function("return " + expression)();
+
+                expression =
+                    (current ** 2).toString();
+
+                updateDisplay();
+
+            }
+
+            catch {
+
+                display.value = "Ошибка";
+                expression = "";
+
+            }
+
+        }
+
+    }
+
+    else if (value === "x³") {
+
+        if (expression !== "") {
+
+            try {
+
+                const current =
+                    Function("return " + expression)();
+
+                expression =
+                    (current ** 3).toString();
+
+                updateDisplay();
+
+            }
+
+            catch {
+
+                display.value = "Ошибка";
+                expression = "";
+
+            }
 
         }
 
@@ -64,12 +176,12 @@ buttons.addEventListener("click", (event) => {
 
         try {
 
-            const result = Function(
-                "return " + expression
-            )();
+            const result =
+                Function("return " + expression)();
 
             expression = result.toString();
-            display.value = expression;
+
+            updateDisplay();
 
         }
 
@@ -102,7 +214,7 @@ buttons.addEventListener("click", (event) => {
 
         }
 
-        display.value = expression;
+        updateDisplay();
 
     }
 
